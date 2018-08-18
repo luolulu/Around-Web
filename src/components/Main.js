7ee225be6ba83a88abf1eae59d845067d78ae5ba
luolulu <luo@LUOdeMacBook-Pro.local>
2018-08-18 11:06:11 -0500

    ReactRouter

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Register } from './Register';
 import { Login } from './Login';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { Home } from './Home';
 
 export class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                <Login/>
+                <Switch>
+                    <Route exact path="/" render={this.getRoot}/>
+                    <Route path="/register" component={Register}/>
+                    <Route path="/login" render={this.getLogin}/>
+                    <Route path="/home" render={this.getHome}/>
+                    <Route render={this.getRoot}/>
+                </Switch>
+
             </div>
         );
     }
